91c469d7024afd7bdd85fbca34e49398d8602ffd
Otilia-Torgby <otorgby@st.ug.edu.gh>
2024-06-01 11:14:10 +0100

    Modified the App.js file

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,20 +1,32 @@
+import React, {useState} from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image,} from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontSize:30,fontWeight:`bold`,}}>Hello , Devs</Text>
+     <Text>14 tasks today</Text>
+      <Image
+        source={require("./Images/image 1.png")}
+        style={{ width: 50, height: 50, marginLeft:300, position:`absolute`,
+          backgroundColor: `#fbf9f7`,borderRadius: 100,}}
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f7f0e8',
+    padding: 30,
+
   },
+ 
 });
+
+            
